14bdf6004208e1e00c2b52ace24d472cf7f0689a
Gregory Lagranha <gregory_lagranha@hotmail.com>
2025-01-24 01:06:08 -0300

    fix: @nestjs/microservices version

--- dist/utils/tenancy.utils.js
+++ dist/utils/tenancy.utils.js
@@ -1,18 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTenantConnectionToken = exports.getTenantModelDefinitionToken = exports.getTenantModelToken = void 0;
+exports.getTenantModelToken = getTenantModelToken;
+exports.getTenantModelDefinitionToken = getTenantModelDefinitionToken;
+exports.getTenantConnectionToken = getTenantConnectionToken;
 const tenancy_constants_1 = require("../tenancy.constants");
 function getTenantModelToken(model) {
     return `${model}Model`;
 }
-exports.getTenantModelToken = getTenantModelToken;
 function getTenantModelDefinitionToken(model) {
     return `${model}Definition`;
 }
-exports.getTenantModelDefinitionToken = getTenantModelDefinitionToken;
 function getTenantConnectionToken(name) {
     return name && name !== tenancy_constants_1.DEFAULT_TENANT_DB_CONNECTION
         ? `${name}TenantConnection`
         : tenancy_constants_1.DEFAULT_TENANT_DB_CONNECTION;
 }
-exports.getTenantConnectionToken = getTenantConnectionToken;
